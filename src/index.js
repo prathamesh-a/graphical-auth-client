f963ba1ff15280f178c86b5e6bf6eebc3b98bf1d
Prathamesh Auti <prathamesh16.auti@gmail.com>
2023-03-03 21:31:55 +0530

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.min.css';
+import {Analytics} from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,5 +12,6 @@ root.render(
   <React.StrictMode>
       <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} newestOnTop={false} rtl={false} theme="dark"/>
       <App />
+      <Analytics />
   </React.StrictMode>
 );
